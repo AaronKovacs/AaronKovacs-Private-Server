1b2245ae53073efd8136f5cf56d60c15a2003c4e
Aaron Kovacs <aaronk4576@gmail.com>
2020-01-15 11:22:22 -0500

    Update config.py

--- source/configuration/config.py
+++ source/configuration/config.py
@@ -4,7 +4,7 @@ import os
 PAGE_SIZE = 20
 
 # Database
-PROD_DB = 'mysql+pymysql://server_user:Server1234@aa6t7orqp8ohxz.cr5a3mpymtml.us-east-1.rds.amazonaws.com:3306/ebdb'
+PROD_DB = os.environ['db']
 
 def DB_URL():
     return PROD_DB
